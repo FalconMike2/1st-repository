85d4b30ee3fc711684ab85feb158ae808197be75
FalconMike2 <mikas.ramanauskas@outlook.com>
2025-12-07 17:04:55 +0200

    Create day2.py
    day two in python

---
+++ day2.py
@@ -0,0 +1,6 @@
+hours = int(input("How many hours? "))
+minutes = int(input("How many minutes? "))
+totalMinutes = minutes+(hours*60)
+totalSeconds = totalMinutes*60
+print("m= ",totalMinutes)
+print("s= ",totalSeconds)
